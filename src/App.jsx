8f820becf38be282eefc625a8f1c1bbbafe3ac66
Lharyzza <lharyzzav@missionreadyhq.com>
2026-06-13 20:30:45 +1200

    feat: added imports into app for new files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects.jsx";
 import Skills from "./components/Skills/Skills";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
